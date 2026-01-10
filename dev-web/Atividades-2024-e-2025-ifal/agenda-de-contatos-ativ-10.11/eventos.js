becc82ed62906f2cded4548fba03b2123974ba92
João Henrique da silva <7joaohenrique5557@gmail.com>
2026-01-10 13:27:24 -0300

    fixing bugs

---
+++ dev-web/Atividades-2024-e-2025-ifal/agenda-de-contatos-ativ-10.11/eventos.js
@@ -0,0 +1,49 @@
+let contatos = [];
+const lista_contatos_div = document.querySelector('.lista_contatos');
+
+const adicionar_contato = () => {
+    const nome = prompt("Qual é o seu nome?")
+    const email = prompt("Qual é o seu email?")
+    const numero = prompt("Qual é o seu numero?")
+
+    const novo_contato = {
+        nome : nome,
+        email : email,
+        numero : numero,
+        id : contatos.length > 0 ? contatos.length + 1: 1
+    }
+    contatos.push(novo_contato)
+    mostrar_contatos()
+}
+
+const mostrar_contatos = () => {
+    lista_contatos_div.innerHTML = ''
+    for (contato of contatos){
+        lista_contatos_div.innerHTML += `
+        <div class="contato" id="contato-${contato.id}">
+            <p>${contato.nome}</p>
+            <p>Email: ${contato.email}</p>
+            <p>Numero: ${contato.numero}</p>
+            <p>ID: ${contato.id}</p>
+        </div>
+        `
+    }
+}
+
+const excluir_contato = () => {
+    const id_contato = parseInt(prompt("Qual o ID do contato que deseja excluir?"))
+
+    i = 0;
+    let achou_contato = false;
+
+    while (achou_contato == false){
+        if (contatos[i].id_contato == contatos.id[id_contato]){
+            contatos.id[id_contato] = null;
+            
+            mostrar_contatos();
+            achou_contato = true;
+        } else{
+            i += 1
+        }
+    }
+}
